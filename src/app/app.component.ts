2993864bedef233c6f3716c1e54210450d87cb3a
Dan Bozaru <appleuser@Apples-MacBook-Pro.local>
2024-02-20 17:30:32 +0200

    MPL-1004 implement planner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-root',
--- src/app/planner/planner.component.ts
+++ src/app/planner/planner.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 
 @Component({
-  selector: 'app-planner', // the selector you'll use in your HTML
-  templateUrl: './planner.component.html', // the HTML template file
-  styleUrls: ['./planner.component.scss'] // the style file
+  selector: 'app-planner',
+  templateUrl: './planner.component.html',
+  styleUrls: ['./planner.component.scss'],
+  standalone: true,
+  imports: [MatInputModule, MatButtonModule, MatTableModule, ReactiveFormsModule]
 })
-export class PlannerComponent {}
+export class PlannerComponent implements OnInit {
+  addForm!: FormGroup;
+  displayedColumns: string[] = ['index', 'name', 'x', 'y'];
+  dataSource = new MatTableDataSource();
+
+  constructor(private formBuilder: FormBuilder) {
+    this.addForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      x: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      y: ['', [Validators.required, Validators.pattern('^[0-9]*$')]]
+    });
+  }
+
+  ngOnInit() {
+    this.setData();
+  }
+
+  private setData(): void {
+    const waypointsData = localStorage.getItem('waypointsData');
+    if (waypointsData) {
+      this.dataSource.data = JSON.parse(waypointsData).waypoints.reverse();
+    }
+  }
+
+  onSubmit(form: FormGroup) {
+    if (form.valid) {
+      const name = form.get('name')?.value;
+      const x = form.get('x')?.value;
+      const y = form.get('y')?.value;
+
+      const parsedStoredObject = JSON.parse(localStorage.getItem('waypointsData') || JSON.stringify({ waypoints: [] }));
+      parsedStoredObject.waypoints.push({ name, x, y });
+      localStorage.setItem('waypointsData', JSON.stringify(parsedStoredObject));
+
+      this.setData();
+
+      form.reset();
+    }
+  }
+}
